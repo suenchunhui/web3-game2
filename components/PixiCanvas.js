7527917fd3e85cbf94ee2cc61dce0ceeb4d10688
kingsmil <wawgickarp@gmail.com>
2023-06-14 13:22:11 +0800

    /mintsong wrapped chakra ui around _app

--- components/PixiCanvas.js
+++ components/PixiCanvas.js
@@ -1,78 +1,45 @@
-"use client";
+"use client"
 import React, { useEffect, useRef } from 'react';
-
-let PIXI;
+import * as PIXI from 'pixi.js';
 
 const PixiCanvas = () => {
     const canvasRef = useRef();
 
     useEffect(() => {
-        import('pixi.js').then((pixi) => {
-            PIXI = pixi;
-
-            const app = new PIXI.Application({ width: 800, height: 600 });
-            canvasRef.current.appendChild(app.view);
-
-            // Button Graphics
-            let buttons = [];
-            let colors = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00];
-            let keys = ['Q', 'W', 'E', 'R'];
-            let keyCodes = [81, 87, 69, 82];
-            let score = 0;
-
-            for(let i = 0; i < 4; i++){
-                let button = new PIXI.Graphics();
-                button.beginFill(colors[i]);
-                button.drawCircle(0, 0, 50);
-                button.endFill();
-                button.x = 100 + i*150;
-                button.y = 300;
-                button.interactive = true;
-                button.buttonMode = true;
-                button.name = keys[i];
-                buttons.push(button);
-                app.stage.addChild(button);
-            }
-
-            let currentButton = null;
-
-            function getRandomButton(){
-                return buttons[Math.floor(Math.random()*4)];
-            }
-
-            function flashButton(button){
-                let originalColor = button._fillStyle.color;
-                button.tint = 0xAAAAAA;
-                setTimeout(() => {
-                    button.tint = originalColor;
-                }, 500);
-                currentButton = button;
-            }
-
-            setInterval(() => {
-                flashButton(getRandomButton());
-            }, 1000);
-
-            window.addEventListener('keydown', function(event) {
-                let keyCode = event.keyCode;
-                if(currentButton && keyCodes.includes(keyCode)){
-                    if(currentButton.name === keys[keyCodes.indexOf(keyCode)]){
-                        score += 1;
-                        console.log("Score: " + score);
-                        if(score === 10) {
-                            console.log("You win!");
-                        }
-                        currentButton = null;
-                    } else {
-                        console.log("Wrong button!");
-                    }
-                }
-            });
-
+        let app = new PIXI.Application({
+            backgroundColor: 0x1099bb,
+            resolution: window.devicePixelRatio || 1,
         });
+
+        canvasRef.current.appendChild(app.view);
+
+        const pianoKeys = [];
+        const numberOfKeys = 14; // 14 keys for two octaves (7 white and 7 black keys)
+        const keyWidth = app.view.width / numberOfKeys/2;
+
+        for (let i = 0; i < numberOfKeys; i++) {
+            let key = new PIXI.Graphics();
+            if (i % 2 === 0) { // White key
+                key.beginFill(0xFFFFFF);
+            } else { // Black key
+                key.beginFill(0x000000);
+            }
+            key.drawRect(i * keyWidth, 0, keyWidth, app.view.height);
+            key.endFill();
+
+            pianoKeys.push(key);
+            app.stage.addChild(key);
+        }
+
+        // Cleanup on unmount
+        return () => {
+            app.stop();
+            app.view.parentNode.removeChild(app.view);
+            app = null;
+        };
     }, []);
 
-    return <div ref={canvasRef} />;
+    return <div ref={canvasRef}></div>;
 };
 
 export default PixiCanvas;
