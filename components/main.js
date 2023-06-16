8828612b4160da67eb0d65e7b347c28fea857fa9
Suen Chun Hui <23443533+suenchunhui@users.noreply.github.com>
2023-06-16 14:52:33 +0800

    ui changes

--- components/main.js
+++ components/main.js
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from 'react'
 
 //...
 
-
+//          <svg className="w-4 h-4 ml-3 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
 
 const Main = () => {
   const { provider, login, logout, user, isLoading, getAccounts, getBalance, signMessage, signTransaction, signAndSendTransaction, web3Auth, chain } = useWeb3Auth();
@@ -20,9 +20,9 @@ const Main = () => {
           onClick={logout} disabled={isLoading} variant="outlined">
           Log Out
         </button>
-        <button disabled={isLoading} className="rounded-full inline-flex items-center h-10 px-5 text-green-100 transition-colors duration-150 bg-green-500 rounded-lg focus:shadow-outline hover:bg-green-800">
-          <span>{userAddress} </span>          <svg className="w-4 h-4 ml-3 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-        </button>
+        <p disabled={isLoading} className="rounded-full inline-flex items-center h-10 px-5 text-green-100 transition-colors duration-150 bg-green-500 rounded-lg focus:shadow-outline hover:bg-green-800">
+          <p>{userAddress}</p>
+        </p>
 
       </div>
     </>
